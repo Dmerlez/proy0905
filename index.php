d474fe225d75d1460a9e4a233f36757212b148d3
cetecom <cetecom@LC08L5721>
2017-05-09 11:26:12 -0300

    Class Producto

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <?php
 include ('lib/Producto.php');
+include ('lib/ConsultaProducto.php');
 
 $oProducto1=new Producto();
 $oProducto1->nombre="Nuevo Producto 001";
 $oProducto1->codigo="001";
 
-$oProducto2=new Producto("Nuevo Producto 002",0,"002");
-$oProducto3=new Producto("Nuevo Producto 003",0,"003");
+$oProducto2=new Producto("Nuevo Producto 002",100000,"002");
+$oProducto3=new Producto("Nuevo Producto 003",250000,"003");
 
 $aProducto[0]=$oProducto1;
 $aProducto[1]=$oProducto2;
@@ -26,10 +27,14 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-            foreach($aProducto as $oPro){
-                echo $oPro->codigo." ".$oPro->nombre;
-                echo "<br>";
-            }
+        $aProductos2=new ConsultaProducto();
+        
+        foreach($aProductos2->Lista() as $oPro){
+            echo $oPro->codigo." ".$oPro->nombre."/".$oPro->precio."/usd: ".$oPro->totalUSD();
+            echo "<br>";
+        }
         ?>
+        
+        
     </body>
 </html>
