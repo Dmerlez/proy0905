8669c75c26e8d2e0bae1088d98c389f62ba5fcb1
cetecom <cetecom@LC08L5721>
2017-05-09 11:30:07 -0300

    Class Producto hola

---
+++ lib/ConsultaProducto.php
@@ -0,0 +1,31 @@
+<?php
+/*include'Producto';*/
+
+class ConsultaProductos{
+    
+    private function Conexion(){
+        $miconn = new mysqli("localhost", "root", "avaras08", "ventas");
+                if ($miconn->connect_errno) {
+                return "Fallo al conectar a MySQL: (" . $miconn->connect_errno . ") " . $miconn->connect_error;
+        }
+        return $miconn;
+    }
+
+    
+    public function Lista(){
+        $sql="SELECT * FROM producto";
+        /*Uso del metodo conexion*/
+         $resultado = $this->Conexion()->query($sql); 
+
+        /*Obtencion de los elementos*/
+         $i=0;
+         while($fila = $resultado->fetch_assoc()){
+            $oProducto=new Producto($fila["nombre"], $fila["precio"], $fila["codigo"]);
+            $aProductos[$i]=$oProducto;
+         }
+         return $aProductos;
+    }
+    
+    
+}
+
